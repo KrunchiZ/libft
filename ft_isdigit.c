6264554b63d1c2de043673b813782061a3221fe3
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2025-05-19 11:45:18 +0800

    added ft_isdigit.c

---
+++ ft_isdigit.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/19 11:39:26 by kchiang           #+#    #+#             */
+/*   Updated: 2025/05/19 11:44:14 by kchiang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// return 1 if c is a digit character
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+/*
+#include <stdio.h>
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		if (ft_isdigit(*(argv[1])))
+			printf("yes");
+		else
+			printf("no");
+	}
+	return (0);
+}
+*/
