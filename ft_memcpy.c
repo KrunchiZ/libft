6b1268068aeace101ff5216e2dca5170321bceb6
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2025-05-19 17:55:06 +0800

    added ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/19 17:32:25 by kchiang           #+#    #+#             */
+/*   Updated: 2025/05/19 17:54:55 by kchiang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+// copy n bytes from src to dest
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*temp_src;
+	unsigned char	*temp_dest;
+
+	temp_src = (unsigned char *)src;
+	temp_dest = (unsigned char *)dest;
+	while (n-- > 0)
+		*temp_dest++ = *temp_src++;
+	return (dest);
+}
