fbe497e819ec3908c7b71a712ff7f3bd843bce17
Kai Zhi Chiang <kchiang@u91z01s05.42kl.edu.my>
2025-05-20 11:24:14 +0800

    added comment in ft_strchr and ft_strrchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,12 +6,13 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 10:20:43 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/20 11:01:38 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/20 11:23:25 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// look for first occurrence of c in s
 char	*ft_strchr(const char *s, int c)
 {
 	char	*temp;
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,12 +6,13 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 10:20:43 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/20 11:03:03 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/20 11:23:42 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// look for last occurrence of c in s
 char	*ft_strrchr(const char *s, int c)
 {
 	char	*last;
