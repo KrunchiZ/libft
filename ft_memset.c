45c3078144b425f430a4df3dfa325005408d4b19
Kai Zhi Chiang <kchiang@u91z01s05.42kl.edu.my>
2025-05-20 17:40:59 +0800

    added ft_ prefix to ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,14 +6,14 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 16:01:12 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/19 17:09:31 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/20 17:40:40 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 // fill first n bytes of memory area pointed by s with constant byte c
-void	*memset(void *s, int c, size_t n)
+void	*ft_memset(void *s, int c, size_t n)
 {
 	unsigned char	*temp;
 
