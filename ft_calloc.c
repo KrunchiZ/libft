cbeb5983606322ee66eb01a62af2656d86b65735
Kai Zhi Chiang <kchiang@u91z01s05.42kl.edu.my>
2025-05-20 17:16:16 +0800

    added ft_calloc.c

---
+++ ft_calloc.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/20 16:10:40 by kchiang           #+#    #+#             */
+/*   Updated: 2025/05/20 17:11:25 by kchiang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/* allocate memory for nmemb elements with size bytes each */
+/* if nmemb or size is 0, return a unique pointer */
+/* if nmemb * size > ULONG_MAX, return NULL */
+void	*calloc(size_t nmemb, size_t size)
+{
+	void	*host;
+
+	if ((ULONG_MAX / nmemb) < size || nmemb == 0 || size == 0)
+		return (NULL);
+	host = malloc(nmemb * size);
+	if (host == NULL)
+		return (NULL);
+	ft_memset(host, 0, nmemb * size);
+	return (host);
+}
