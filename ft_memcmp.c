9585c867193a1a9cfb3d1fb7d6d8074fe4a698a3
Kai Zhi Chiang <kchiang@u91z01s05.42kl.edu.my>
2025-05-20 17:35:46 +0800

    added ft_ prefix in ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,14 +6,14 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 12:21:22 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/20 12:44:40 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/20 17:35:32 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /* Compare first n bytes of s1 and s2 as unsigned char */
-int	memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char	*tmp_s1;
 	unsigned char	*tmp_s2;
