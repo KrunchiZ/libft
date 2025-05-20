00ff89c30f4d3a5f24526faae869eb4d1ea3ab24
Kai Zhi Chiang <kchiang@u91z01s05.42kl.edu.my>
2025-05-20 19:24:04 +0800

    fixed ft_memcpy prototype typo in libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 16:52:05 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/20 17:49:40 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/20 19:23:40 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strrchr(const char *s, int c);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_memchr(const void *s, int c, size_t n);
-void	*ft_memcpy(void *dest, const void 8src, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
