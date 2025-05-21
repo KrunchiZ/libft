5546f833cc872c71a9e57096a5ca7f8eafd74ba9
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-05-21 17:25:07 +0800

    fixed ft_substr to truncate len if len goes beyond src[start]

---
+++ ft_substr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/21 14:45:41 by kchiang           #+#    #+#             */
+/*   Updated: 2025/05/21 17:13:39 by kchiang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/* return a substring starting the start index of s */
+/* len is the MAX string length of the substring    */
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	char	*host;
+	size_t	srclen;
+
+	srclen = ft_strlen(s);
+	if (len == 0 || srclen < start)
+		return (ft_calloc(1, 1));
+	if (len > srclen - start)
+		len = srclen - start;
+	host = (char *)malloc(len + 1);
+	if (host == NULL)
+		return (NULL);
+	srclen = ft_strlcpy(host, &s[start], len + 1);
+	return (host);
+}
