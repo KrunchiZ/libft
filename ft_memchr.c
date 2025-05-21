7bf98c900bad5614c868f8acf1abf9fbf44762a1
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-05-21 11:01:45 +0800

    fixed ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 11:22:11 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/20 12:20:20 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/21 10:52:04 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,12 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	unsigned char	*tmp;
 
 	tmp = (unsigned char *)s;
-	while (n > 0 && *tmp)
+	while (n > 0)
 	{
 		if (*tmp == (unsigned char)c)
 			return ((void *)tmp);
 		tmp++;
 		n--;
 	}
-	if (n && *tmp == (unsigned char)c)
-		return ((void *)tmp);
 	return (NULL);
 }
