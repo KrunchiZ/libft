835d045ff7b89710179a1e4829dc4f2ccda4e38f
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-05-21 19:33:27 +0800

    added ft_strtrim in libft.h and Makefile

--- Makefile
+++ Makefile
@@ -5,7 +5,8 @@ SRCS := ft_atoi		ft_bzero	ft_calloc	ft_isalnum	ft_isalpha	\
 		ft_isascii	ft_isdigit	ft_isprint	ft_memchr	ft_memcmp	\
 		ft_memcpy	ft_memmove	ft_memset	ft_strchr	ft_strdup	\
 		ft_strlcat	ft_strlcpy	ft_strlen	ft_strncmp	ft_strnstr	\
-		ft_strrchr	ft_tolower	ft_toupper	ft_substr	ft_strjoin
+		ft_strrchr	ft_tolower	ft_toupper	ft_substr	ft_strjoin	\
+		ft_strtrim
 OBJS := $(addsuffix .o, $(SRCS))
 
 .PHONY: all fclean clean re
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 16:52:05 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/21 18:25:52 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/21 19:32:49 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ char	*ft_strdup(const char *src);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strtrim(const char *s1, const char *set);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_memchr(const void *s, int c, size_t n);
