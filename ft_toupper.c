19b057180093cd15c40c87aecf7b525e047457db
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2025-05-21 22:17:09 +0800

    removed unused variable in ft_toupper

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,16 +6,13 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 10:04:15 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/21 22:08:08 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/21 22:16:35 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // convert lowercase to uppercase
 int	ft_toupper(int c)
 {
-	unsigned char	ch;
-
-	c = (unsigned char)c;
 	if (c >= 'a' && c <= 'z')
 		c -= 32;
 	return (c);
