2314bf57ec20899472dc23497c286de1c3b35df2
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2025-05-21 22:15:26 +0800

    fixed return type error in ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 10:37:22 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/21 22:06:32 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/21 22:14:41 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (n-- > 0 && (*s1 || *s2))
 	{
 		if (*s1 - *s2)
-			return ((unsigned char *)*s1 - (unsigned char *)*s2);
+			return ((unsigned char)*s1 - (unsigned char)*s2);
 		s1++;
 		s2++;
 	}
