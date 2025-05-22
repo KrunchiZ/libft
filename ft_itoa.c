8d108806a222b670de8d59ed500fb8f430719f13
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-05-22 15:48:03 +0800

    ft_itoa first pass

---
+++ ft_itoa.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/22 14:53:30 by kchiang           #+#    #+#             */
+/*   Updated: 2025/05/22 15:46:54 by kchiang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_itoa(int n)
+{
+	char	tmp[12];
+	int		depth;
+	int		is_neg;
+
+	if (n == 0)
+		return (ft_strdup("0"));
+	if (n == INT_MIN)
+		return (ft_strdup("-2147483648"));
+	depth = 11;
+	tmp[depth--] = '\0';
+	if (n < 0)
+	{
+		is_neg = 1;
+		n = -n;
+	}
+	while (n)
+	{
+		tmp[depth--] = n % 10 + '0';
+		n /= 10;
+	}
+	if (is_neg)
+		tmp[depth] = '-';
+	else
+		depth++;
+	return (ft_strdup(&tmp[depth]));
+}
