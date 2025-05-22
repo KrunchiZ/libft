4431e910d7547d77658d8e784304f2650f7d3740
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-05-22 16:09:37 +0800

    reduced a line in ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 14:53:30 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/22 15:55:15 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/22 16:09:10 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ char	*ft_itoa(int n)
 	if (n == INT_MIN)
 		return (ft_strdup("-2147483648"));
 	is_neg = 0;
-	depth = 11;
-	tmp[depth--] = '\0';
+	depth = 12;
+	tmp[--depth] = '\0';
 	if (n < 0)
 	{
 		is_neg = 1;
@@ -32,10 +32,9 @@ char	*ft_itoa(int n)
 	}
 	while (n)
 	{
-		tmp[depth--] = n % 10 + '0';
+		tmp[--depth] = n % 10 + '0';
 		n /= 10;
 	}
-	depth++;
 	if (is_neg)
 		tmp[--depth] = '-';
 	return (ft_strdup(&tmp[depth]));
