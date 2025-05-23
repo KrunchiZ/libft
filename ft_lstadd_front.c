353954d0f50a1e298e714f0af5e5ab94d8045fb5
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-05-23 23:06:56 +0800

    check for null new node in ft_lstadd_front

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 15:25:26 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/23 15:52:33 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/23 23:06:30 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	new->next = *lst;
-	*lst = new;
+	if (new)
+	{
+		new->next = *lst;
+		*lst = new;
+	}
 	return ;
 }
