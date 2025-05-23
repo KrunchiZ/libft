55d34d38f86d13850b3e5bb52fc30bbe1ef902d9
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-05-23 16:52:39 +0800

    fixed null checker in ft_lstsize again.

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 15:56:02 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/23 16:49:57 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/23 16:52:11 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_lstsize(t_list *lst)
 	int	size;
 
 	size = 0;
-	if (*lst)
+	if (lst)
 	{
 		size++;
 		lst = lst->next;
