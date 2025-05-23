bc7fbc46a8dc8013490a976e315f439bfe06a850
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-05-23 19:07:25 +0800

    ft_lstadd_back.c

---
+++ ft_lstadd_back.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/23 17:14:30 by kchiang           #+#    #+#             */
+/*   Updated: 2025/05/23 19:04:01 by kchiang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/* Added new to the back of lst */
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last_node;
+
+	last_node = ft_lstlast(*lst);
+	last_node->next = new;
+	return ;
+}
