594b1c519a9bf767afcb17b6d605f70462d4a185
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-05-23 15:07:46 +0800

    added ft_lstnew.c

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,12 +6,18 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 12:50:54 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/23 13:16:42 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/23 15:06:59 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/* return a struct node initialized with content. */
 t_list	*ft_lstnew(void *content)
 {
+	t_list	*new_node;
+
+	new_node = malloc(sizeof(t_list));
+	*new_node = {.content = content, .next == NULL};
+	return (new_node);
 }
