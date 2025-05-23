094eb4c2e78d634ef59754b757770a42b7d5c0a8
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-05-23 19:52:57 +0800

    added ft_lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 17:14:30 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/23 19:04:01 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/23 19:51:13 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	t_list	*last_node;
 
 	last_node = ft_lstlast(*lst);
-	last_node->next = new;
+	if (last_node == NULL)
+		*lst = new;
+	else
+		last_node->next = new;
 	return ;
 }
