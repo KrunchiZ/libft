751f2b76bdc5559aa0349b1f7bfefa741a9f0e46
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-05-23 22:07:40 +0800

    ft_lstmap.c first pass

---
+++ ft_lstmap.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/23 21:40:07 by kchiang           #+#    #+#             */
+/*   Updated: 2025/05/23 22:04:29 by kchiang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/* Apply function f on the content of each node in lst */
+/* Copy the whole lst to a new list */
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_node;
+	t_list	*first_node;
+
+	first_node = NULL;
+	if (!lst)
+		return (NULL);
+	while (lst)
+	{
+		new_node = ft_lstnew(f(lst->content));
+		if (new_node == NULL)
+			return (NULL);
+		ft_lstadd_back(&first_node, new_node);
+		lst = lst->next;
+	}
+	ft_lstadd_back(&first_node, NULL);
+	return (first_node);
+}
