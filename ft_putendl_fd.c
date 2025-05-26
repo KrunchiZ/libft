0acbe77a0ba99ded196d6bbfcded1045f92f2c48
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-05-26 15:05:00 +0800

    optimized ft_putstr and ft_putendl

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 11:13:00 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/23 13:30:34 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/26 14:59:21 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,7 @@
 // write string s to the file descriptor fd followed by newline
 void	ft_putendl_fd(char *s, int fd)
 {
-	while (*s)
-		write(fd, s++, 1);
+	write(fd, s, ft_strlen(s));
 	write(fd, "\n", 1);
 	return ;
 }
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 11:06:00 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/23 13:30:10 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/26 14:58:07 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 // write string s to the file descriptor fd
 void	ft_putstr_fd(char *s, int fd)
 {
-	while (*s)
-		write(fd, s++, 1);
+	write(fd, s, ft_strlen(s));
 	return ;
 }
