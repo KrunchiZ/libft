6e65e39ee32fe6b572f10b810ae3c64298fae6f4
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-05-26 15:48:39 +0800

    optimized ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,26 +6,11 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 09:19:41 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/24 17:28:22 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/26 15:47:26 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/* count word number in s using c as separator */
-static int	ft_wordcount(const char *s, char c, int word)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
-			word++;
-		i++;
-	}
-	return (word);
-}
 
 /* ft_strlen but with a separator c check as well */
 static int	ft_wordlen(const char *s, const char c)
@@ -39,15 +24,6 @@ static int	ft_wordlen(const char *s, const char c)
 		s++;
 	}
 	return (len);
-}
-
-/* free the host array from depth towards the front */
-static void	ft_freehost(char **host, int depth)
-{
-	while (depth >= 0)
-		free(host[depth--]);
-	free(host);
-	return ;
 }
 
 /* move across s and allocata memory for every word */
@@ -67,7 +43,9 @@ static int	ft_fillhost(char **host, const char *s, char c, int word)
 			host[depth] = ft_calloc(wordlen + 1, 1);
 			if (host == NULL)
 			{
-				ft_freehost(host, depth);
+				while (depth >= 0)
+					free(host[depth--]);
+				free(host);
 				return (0);
 			}
 			ft_strlcpy(host[depth], &s[i], wordlen + 1);
@@ -81,6 +59,7 @@ static int	ft_fillhost(char **host, const char *s, char c, int word)
 /* split s into individual word in an array and return it */
 char	**ft_split(const char *s, char c)
 {
+	int		i;
 	int		word;
 	char	**host;
 
@@ -88,10 +67,17 @@ char	**ft_split(const char *s, char c)
 		return (NULL);
 	if (!*s)
 		return (ft_calloc(1, sizeof(char *)));
-	word = ft_wordcount(s, c, 0);
-	host = ft_calloc(word + 1, sizeof(char *));
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+			word++;
+		i++;
+	}
+	host = malloc((word + 1) * sizeof(char *));
 	if (!host)
 		return (NULL);
+	host[word] = NULL;
 	if (ft_fillhost(host, s, c, word))
 		return (host);
 	return (NULL);
